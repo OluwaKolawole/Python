5b2cc589a6aed4be07c60cbf67b6457f6ef12922
omosorosoke@gmail.com <ojokolawoleshola@gmail.com>
2023-08-02 19:22:10 +0300

    added new file

--- 0-python.py
+++ 0-python.py
@@ -1,1 +1,6 @@
-print("kola")
+def family_member(father, mother, **children):
+    print(father + " is the haed of the family")
+    print(mother + " is the wife")
+    for c in children.values():
+        print("The children are: " + "\n" + c)
+family_member(father = "kola", mother = "bisi", daugther = "iremide")
